96d2094d102368b813832091877839b0164b9a44
AMANDA BOTELHO DE MORAES <41346828865@jpmorgan.sp>
2018-02-07 22:00:14 -0200

    AutenticacaoCookies

---
+++ Dados/CodeFirstBanco.cs
@@ -0,0 +1,34 @@
+using AutenticacaoEFCookie.Models;
+
+namespace AutenticacaoEFCookie.Dados
+{
+    public class CodeFirstBanco
+    {
+        public static void Inicializar(AutenticacaoContext contexto) {
+            contexto.Database.EnsureCreated(); //Cria o banco se ele não existir
+
+            var usuario = new Usuario(){
+                Nome="Amanda",
+                Email="amanda@gmail.com",
+                Senha="123456"
+            };
+
+            contexto.Usuarios.Add(usuario);
+            
+            var permissao = new Permissao(){
+                Nome="Financeiro"
+            };
+
+            contexto.Permissoes.Add(permissao);
+
+            var usuariosPermissoes = new UsuarioPermissao(){
+                IdUsuario = usuario.IdUsuario,
+                IdPermissao = permissao.IdPermissao
+            };
+            
+            contexto.UsuariosPermissoes.Add(usuariosPermissoes);
+            
+            contexto.SaveChanges();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,19 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutenticacaoEFCookie.Dados;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AutenticacaoEFCookie
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration){
+            this.Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AutenticacaoContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("BancoAutenticacao")));
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                            .AddCookie(options => { options.LoginPath = "Conta/Login"; }); //Acessando outras paginas, redireciona para essa pagina
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +40,13 @@ namespace AutenticacaoEFCookie
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseAuthentication();
+            app.UseMvc( routes =>{
+                routes.MapRoute(
+                    name:"default",
+                    template:"{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
